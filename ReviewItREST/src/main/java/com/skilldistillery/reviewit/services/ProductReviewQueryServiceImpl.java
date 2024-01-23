bcd0f6d10e95e9da7c8dd74a384ce72d4cadb9fa
Jacob Snell <jake.r.snell@gmail.com>
2024-01-22 23:04:43 -0700

    fix typo

--- ReviewItREST/src/main/java/com/skilldistillery/reviewit/services/ProductReviewQueryServiceImpl.java
+++ ReviewItREST/src/main/java/com/skilldistillery/reviewit/services/ProductReviewQueryServiceImpl.java
@@ -3,12 +3,14 @@ package com.skilldistillery.reviewit.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.reviewit.entities.ProductReview;
 import com.skilldistillery.reviewit.exceptions.EntityDoesNotExistException;
 import com.skilldistillery.reviewit.repositories.ProductRepository;
 import com.skilldistillery.reviewit.repositories.ProductReviewRepository;
 
+@Service
 public class ProductReviewQueryServiceImpl implements ProductReviewQueryService {
 
 	@Autowired
