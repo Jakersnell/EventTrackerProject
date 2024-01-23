92e8f294ea6ae18ce3964567b8fe1e6382f9dc06
Jacob Snell <jake.r.snell@gmail.com>
2024-01-22 22:59:54 -0700

    Refactor PublicCategoryController to ResponseEntity based method.

--- ReviewItREST/src/main/java/com/skilldistillery/reviewit/controllers/PublicCategoryController.java
+++ ReviewItREST/src/main/java/com/skilldistillery/reviewit/controllers/PublicCategoryController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +22,20 @@ import jakarta.servlet.http.HttpServletResponse;
 @CrossOrigin({ "*", "http://localhost/" })
 @RequestMapping({ "api/categories" })
 @RestController
-public class PublicCategoryController extends BaseController {
+public class PublicCategoryController {
 
 	@Autowired
 	private CategoryQueryService cqs;
 
 	@GetMapping
-	private PageDTO<CategoryDTO> getAll(@RequestParam(name = "pageNum") int pageNum,
+	private ResponseEntity<PageDTO<CategoryDTO>> getAll(@RequestParam(name = "pageNum") int pageNum,
 			@RequestParam(name = "pageSize") int pageSize,
 			@RequestParam(name = "searchQuery", required = false) String searchQuery,
 			@RequestParam(name = "excludedCategories", required = false) Set<Category> excludedCategories,
 			HttpServletResponse res) {
-		return tryFailableAction(() -> {
-			Page<Category> page = cqs.getPageOfCategories(pageNum, pageSize, searchQuery, excludedCategories, true);
-			List<CategoryDTO> categories = page.getContent().stream().map(CategoryDTO::new).toList();
-			return new PageDTO<>(page, categories, searchQuery);
-		}, res);
-
+		Page<Category> page = cqs.getPageOfCategories(pageNum, pageSize, searchQuery, excludedCategories, true);
+		List<CategoryDTO> categories = page.getContent().stream().map(CategoryDTO::new).toList();
+		return ResponseEntity.ok(new PageDTO<>(page, categories, searchQuery));
 	}
 
 }
